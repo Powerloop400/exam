af4a5e3f0eb4bf85830defc23c467285aa91ac44
Abdurrahman Balogun <mabdurrahman.balogun@gmail.com>
2023-10-25 11:49:24 +0100

    final i hope

--- routes/blogs.js
+++ routes/blogs.js
@@ -175,9 +175,12 @@ blogRouter.get('/myblogs', (req, res) => {
 
 
 blogRouter.get('/drafts', authenticate, (req, res) => {
-    const page = req.query.page || 1; // Default to page 1
-    const perPage = req.query.perPage || 20; // Default to 20 blogs per page
+    const page = req.query.page || 1;
+    const perPage = req.query.perPage || 20; 
+      const token = req.cookies.token;
+
     const { userId } = jwt.verify(token, secretkey);
+
 
     // Log that the '/drafts' route has been accessed
     logger.info('GET /drafts route accessed.');
